47eff7e534d510ced7d23b76b11a0d4d162f54b9
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-11-04 04:08:51 -0400

    Agregando La Fecha para el Calendario

---
+++ app/src/main/java/edu/ucne/taskmaster/data/di/DateAdater.kt
@@ -0,0 +1,21 @@
+package edu.ucne.taskmaster.data.di
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class DateAdater {
+    private val dateformat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US)
+
+    @ToJson
+    fun dateToJson(value: Date?): String? {
+        return value?.let { dateformat.format(it) }
+    }
+
+    @FromJson
+    fun fromJson(value: String?): Date? {
+        return value?.let { dateformat.parse(it) }
+    }
+}
