6fd2aa74b54393b395c84bde2c739e636e3c5431
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-11-17 18:56:35 -0400

    Actualizando Los Dao

--- app/src/main/java/edu/ucne/taskmaster/data/local/dao/TaskDao.kt
+++ app/src/main/java/edu/ucne/taskmaster/data/local/dao/TaskDao.kt
@@ -2,33 +2,48 @@ package edu.ucne.taskmaster.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Upsert
 import edu.ucne.taskmaster.data.local.entities.TaskEntity
 
 @Dao
 interface TaskDao {
-    @Upsert()
-    suspend fun SaveTask(task: TaskEntity)
+    @Upsert
+    suspend fun saveTask(task: TaskEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun save(task: TaskEntity) // todo save para insertar o reemplazar
 
     @Query(
         """
         SELECT * FROM Tasks
         WHERE TaskId = :id
-        Limit 1
+        LIMIT 1
         """
     )
-    suspend fun getTask(id: Int): TaskEntity
+    suspend fun getTask(id: Int): TaskEntity?
 
     @Query(
         """
         SELECT * FROM Tasks
-        
         """
     )
     suspend fun getAllTask(): List<TaskEntity>
 
     @Delete
     suspend fun DeleteTask(task: TaskEntity)
+
+    @Query(
+        """
+        DELETE FROM Tasks
+        WHERE TaskId = :id
+        """
+    )
+    suspend fun delete(id: Int)
+
+    @Query("DELETE FROM Tasks WHERE TaskId = :id")
+    suspend fun deleteById(id: Int)
 
 }
--- app/src/main/java/edu/ucne/taskmaster/data/local/dao/UserDao.kt
+++ app/src/main/java/edu/ucne/taskmaster/data/local/dao/UserDao.kt
@@ -1,15 +1,30 @@
 package edu.ucne.taskmaster.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import edu.ucne.taskmaster.data.local.entities.UserEntity
 
 @Dao
 interface UserDao {
+    // Obtener todos los usuarios
     @Query("SELECT * FROM User")
     suspend fun getAll(): List<UserEntity>
 
+    // Obtener un usuario por ID
+    @Query("SELECT * FROM User WHERE userId = :id LIMIT 1")
+    suspend fun getUser(id: Int): UserEntity?
+
+    // Guardar o actualizar un usuario
     @Upsert
     suspend fun saveUser(user: UserEntity)
+
+    // Eliminar un usuario
+    @Delete
+    suspend fun delete(user: UserEntity)
+
+    // Eliminar un usuario por ID
+    @Query("DELETE FROM User WHERE userId = :id")
+    suspend fun deleteUserById(id: Int)
 }
