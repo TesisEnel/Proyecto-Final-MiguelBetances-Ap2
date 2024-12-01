31f7078259a293bef447ec56d04d2de935a175b4
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-11-30 21:02:57 -0400

    Modificando NavHost

--- app/src/main/java/edu/ucne/taskmaster/navigation/NavHostApp.kt
+++ app/src/main/java/edu/ucne/taskmaster/navigation/NavHostApp.kt
@@ -5,7 +5,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import edu.ucne.taskmaster.presentation.Calendar.CalendarScreen
+import edu.ucne.taskmaster.presentation.Label.LabelScreen
+import edu.ucne.taskmaster.presentation.TaskList.TaskListScreen
+import edu.ucne.taskmaster.presentation.TaskList.TaskScreen
 
 @Composable
 fun NavHostApp(navHostController: NavHostController, drawerState: DrawerState) {
@@ -15,6 +19,33 @@ fun NavHostApp(navHostController: NavHostController, drawerState: DrawerState) {
     ) {
         composable<Screen.Calendar> {
             CalendarScreen()
+        }
+        composable<Screen.TaskList> {
+            TaskListScreen(
+                createTask = {
+                    navHostController.navigate(Screen.Task(0))
+                },
+                gotoTask = {
+                    navHostController.navigate(Screen.Task(it))
+                },
+                gotoEditTask = {
+                    navHostController.navigate(Screen.Task(it))
+                }
+            )
+        }
+
+        composable<Screen.Task> {
+            val args = it.toRoute<Screen.Task>()
+            TaskScreen(
+                taskId = args.id,
+                goBackToTaskList = {
+                    navHostController.navigate(Screen.TaskList)
+                }
+            )
+        }
+
+        composable<Screen.Label> {
+            LabelScreen()
         }
 
     }
