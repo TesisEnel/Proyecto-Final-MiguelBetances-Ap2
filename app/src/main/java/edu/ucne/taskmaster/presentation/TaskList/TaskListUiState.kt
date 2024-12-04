9277286b4acae1a44c66207f4b3442f2c9dd1564
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-12-04 01:46:52 -0400

    Agregando Las Prioridades

--- app/src/main/java/edu/ucne/taskmaster/presentation/TaskList/TaskListUiState.kt
+++ app/src/main/java/edu/ucne/taskmaster/presentation/TaskList/TaskListUiState.kt
@@ -1,5 +1,7 @@
 package edu.ucne.taskmaster.presentation.TaskList
 
+import edu.ucne.taskmaster.data.local.entities.LabelEntity
+import edu.ucne.taskmaster.data.local.entities.TaskEntity
 import java.util.Date
 
 data class TaskListUIState(
@@ -11,5 +13,27 @@ data class TaskListUIState(
     val priority: Int = 0,
     val title: String = "",
     val tasks: List<Tasks> = emptyList(),
-    val error: String? = null
+    val error: String? = null,
+    val showModal: Boolean = false,
+    val orderBy: List<String> = listOf("Created Date", "Due Date", "Priority"),
+    val currentOrder: Int = 0,
+    val priorityOptions: List<String> = listOf(
+        "Urgente",
+        "Alta",
+        "Media",
+        "Baja",
+        "Sin prisa"
+    ),
+    val labels: List<LabelEntity> = emptyList(),
+    val labelsSelected: List<LabelEntity> = emptyList(),
+    val labelsDescription: List<String> = emptyList()
+)
+
+fun TaskListUIState.toEntity() = TaskEntity(
+    createdDate = createdDate,
+    description = description,
+    dueDate = dueDate,
+    priority = priority,
+    title = title,
+    taskId = if (id == 0) null else id,
 )
